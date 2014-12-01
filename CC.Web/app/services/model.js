625d7bad9312b243e91751a92a0d52ffe04e8ad2
sjmcinnes <sjmcinnes@yahoo.co.uk>
2014-12-01 09:19:22 +0000

    chapter4

---
+++ CC.Web/app/services/model.js
@@ -0,0 +1,68 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'model';
+    angular
+        .module('app')
+        .factory(serviceId, model);
+    
+    function model() {
+        var service = {
+            configureMetaDataStore: configureMetaDataStore
+        };
+
+        return service;
+
+        function configureMetaDataStore(metadataStore) {
+            //register session - tags
+            registerSession(metadataStore);
+
+            //register person - fullname
+            registerPerson(metadataStore);
+
+            //register timeslot - name
+            registerTimeSlot(metadataStore);
+        }
+
+        function registerPerson(metadataStore) {
+            metadataStore.registerEntityTypeCtor('Person', Person);
+
+            function Person() { }
+
+            Object.defineProperty(Person.prototype, 'fullName', {
+                get: function () {
+                    return this.lastName ? this.firstName + ' ' + this.lastName : this.firstName;
+                }
+            });
+        }
+
+        function registerSession(metadataStore) {
+            metadataStore.registerEntityTypeCtor('Session', Session);
+
+            function Session() { }
+
+            Object.defineProperty(Session.prototype, 'tagsFormatted', {
+                get: function () {
+                    return this.tags ? this.tags.replace(/\|/g, ', ') : this.tags;
+                },
+                set: function(value) {
+                    this.tags = value.replace(/\, /g, '|');
+                }
+            });
+        }
+
+        function registerTimeSlot(metadataStore) {
+            metadataStore.registerEntityTypeCtor('TimeSlot', TimeSlot);
+
+            function TimeSlot() { }
+
+            Object.defineProperty(TimeSlot.prototype, 'name', {
+                get: function () {
+                    var start = this.start;
+                    var value = moment.utc(start).format('ddd hh:mm a');
+                    return value;
+                }
+            });
+        }
+    }
+})();
---
+++ CC.Web/app/sessions/sessions.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'sessions';
+
+    angular
+        .module('app')
+        .controller('sessions', sessions);
+
+    sessions.$inject = ['common','datacontext']; 
+
+    function sessions(common, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'Sessions';
+        vm.sessions = [];
+        activate();
+
+        function activate() {
+            common.activateController([getSessions()], controllerId)
+                .then(function () { log('Activated Sessions View'); });
+        }
+
+        function getSessions() {
+            return datacontext.getSessionPartials().then(function (data) {
+                return vm.sessions = data;
+            });
+        }
+    }
+})();
